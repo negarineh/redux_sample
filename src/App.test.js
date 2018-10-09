c2302b19ff66a7d2b4190dca3ccc1acc7b3e1f09
negarineh <27172694+negarineh@users.noreply.github.com>
2018-10-10 10:56:53 +1100

    add provider to App.test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import {App} from './App';
+import { store } from '../src/_helpers';
+import { Provider } from 'react-redux';
+
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter()});
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Provider store={store}><App /></Provider>, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
